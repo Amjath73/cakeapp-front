8b791454f2426aa073aa170a48d107dcde2848d9
Amjath73 <amjathsanker4@gmail.com>
2024-06-13 08:33:28 +0530

    start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import AddCake from './components/AddCake';
+
+function App() {
+  return (
+    <div>
+      <AddCake/>
+    </div>
+  );
+}
+
+export default App;
+
